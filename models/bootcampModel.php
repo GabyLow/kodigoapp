90a9da2676d06e9253ff949ad7d3aca867d66041
GabyLow <127358083+GabyLow@users.noreply.github.com>
2023-07-28 20:06:37 -0600

    :sparkles: fix:host link

--- models/bootcampModel.php
+++ models/bootcampModel.php
@@ -5,7 +5,7 @@ require_once './config.php';
        private $conn;
 
        public function __construct(){
-           $this->conn = new mysqli('127.0.0.1', 'root', '', 'kodigo');
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
            if ($this->conn->connect_error){
                die("Connection failed: " . $this->conn->connect_error);
            }
